81fbbdafb1e377f993047506ac8bf38b4218ca9a
ggibamede <gibamedeirosgc@gmail.com>
2021-03-20 23:45:11 -0300

    feature(page): Dropdown de categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
+const Category = require("../categories/Category");
 router.get("/articles",(req , res )=>{
     res.send("Artigos aqui ")
 });
 
+//Criação de Artigos
 router.get("/admin/articles/new",(req , res)=>{
-    res.render("admin/articles/new")
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new" , {categories})
+    })
 });
 
 
