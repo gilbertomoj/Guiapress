f7af9f3ea8a5cea235c66891b68125ce7834f26b
ggibamede <gibamedeirosgc@gmail.com>
2021-03-24 00:03:06 -0300

    feature(server): Cadastro de usuários / Configuração do bcrypt

--- user/UsersController.js
+++ user/UsersController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
-
+const bcrypt = require("bcryptjs")
 
 router.get("/admin/users",(req , res)=>{
     res.send("Listagem")
@@ -14,6 +14,22 @@ router.get("/admin/users/create", (req , res)=>{
 router.post("/users/create",(req, res)=>{
     var email = req.body.email;
     var password = req.body.password;
-    res.send({ email, password})
+
+    //Configuração do Bcrypt
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email,
+        password: hash
+    }).then(()=>{
+        res.redirect("/")
+    }).catch(err =>{
+        res.redirect("/") 
+    })
+
+
+
+
 })
 module.exports = router;
